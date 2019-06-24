da50ce1cdb502b5eb8e2aa44665ed69288b0d938
Chris Crutchfield <ccrutchf@ucsd.edu>
2019-06-24 08:56:15 -0700

    initial checkin of using image registration/creating a model of the moving background.

---
+++ registration.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+
+MAX_FEATURES = 500
+GOOD_MATCH_PERCENT = 0.15
+
+def register(frame1, frame2):
+    orb = cv2.ORB_create(MAX_FEATURES)
+
+    keypoints1, descriptors1 = orb.detectAndCompute(frame1, None)
+    keypoints2, descriptors2 = orb.detectAndCompute(frame2, None)
+
+    # Match features.
+    matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
+    matches = matcher.match(descriptors1, descriptors2, None)
+    
+    # Sort matches by score
+    matches.sort(key=lambda x: x.distance, reverse=False)
+    
+    # Remove not so good matches
+    numGoodMatches = int(len(matches) * GOOD_MATCH_PERCENT)
+    matches = matches[:numGoodMatches]
+    
+    # Extract location of good matches
+    points1 = np.zeros((len(matches), 2), dtype=np.float32)
+    points2 = np.zeros((len(matches), 2), dtype=np.float32)
+    
+    for i, match in enumerate(matches):
+        points1[i, :] = keypoints1[match.queryIdx].pt
+        points2[i, :] = keypoints2[match.trainIdx].pt
+    
+    # Find homography
+    h, mask = cv2.findHomography(points1, points2, cv2.RANSAC)
+
+    return h
