99e40ae97158d95b7e24e2fad6618aa7dbf1b559
Anh Ngo <adn057@ucsd.edu>
2019-07-26 21:50:16 -0700

    turn project into installable python package

---
+++ baboon_tracking/__init__.py
@@ -0,0 +1,1 @@
+from .BaboonTracker import BaboonTracker
---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name='BaboonTracking',
+        version='0.1',
+        packages=['baboon_tracking'],
+        author='Anh Ngo',
+        author_email='adn057@ucsd.edu',
+        description='A package to interchangeably test image detection and tracking methods/algorthms/implementations',
+        long_description=long_description,
+        url='https://github.com/UCSD-E4E/baboon-tracking',
+        license='MIT',
+)
