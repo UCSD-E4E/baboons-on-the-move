d6fc16963eff8679532d867fb8afbb9ceced1b40
Alvin Wang <alvinwang922@gmail.com>
2020-04-25 20:41:47 -0700

    Implemented panda output coordinates

--- scripts/getCentroids.py
+++ scripts/getCentroids.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 import cv2
+import pandas as pd
+from pandas import Series, DataFrame
 import numpy as np
 import argparse
 
 parser = argparse.ArgumentParser(description="Find contours and centroids")
 parser.add_argument('input', help='Source video')
-parser.add_argument('-o', '--output', help='Output video', default='output.mp4')
+parser.add_argument('-o', '--output', help='Output video',
+                    default='output.mp4')
 
 args = parser.parse_args()
+
+# array to store coordinates
+coordinates = []
 
 # video capture (reader)
 cap = cv2.VideoCapture(args.input)
@@ -52,19 +58,21 @@ while(cap.isOpened()):
     gray = cv2.GaussianBlur(gray, (5, 5), 0)
 
     # Delete this block when blobs are cohesive =========
-    gray = cv2.erode(gray , erosion_kernel, iterations=1)
+    gray = cv2.erode(gray, erosion_kernel, iterations=1)
     gray = cv2.dilate(gray, dilation_kernel, iterations=1)
     # ===================================================
 
     # contour detection
     ret, thresh = cv2.threshold(gray, 127, 255, 0)
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(
+        thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     # blob detection
     keypoints = detector.detect(gray)
-    
+
     frame = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
-    frame = cv2.drawKeypoints(frame, keypoints, np.array([]), (0, 255, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    frame = cv2.drawKeypoints(frame, keypoints, np.array(
+        []), (0, 255, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
     for c in contours:
         # compute the center of the contour
@@ -76,9 +84,13 @@ while(cap.isOpened()):
         else:
             cX, cY = 0, 0
         # draw the contour and center of the shape on the image
+        coordinates.append([cX, cY])
         cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
         cv2.circle(frame, (cX, cY), 2, (0, 0, 255), -1)
-     
+
+    df = pd.DataFrame.from_records(locations)
+    df.columns = ['x', 'y']
+
     cv2.imshow('frame', frame)
     out.write(frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
