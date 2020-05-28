833cc9f4588d0d6c164b3e51c71f735f80f21bb6
jbk001 <guppykang@gmail.com>
2020-05-27 17:03:58 -0700

    ready for predict function

--- baboon_tracking/models/particle_filter/predict.py
+++ baboon_tracking/models/particle_filter/predict.py
@@ -1,6 +1,5 @@
 import code
 import numpy as np
-import argparse
 import ntpath
 from pathlib import Path
 import os
@@ -9,17 +8,12 @@ from baseline_model import *
 
 root_path = Path('./../../..')
 
-parser = argparse.ArgumentParser(description='Predicts the next state given a motion model')
-parser.add_argument('--model', '-m', default=root_path / 'baboon_tracking' / 'models' / 'particle_filter' / 'net.pth')
-parser.add_argument('--input_dim', '-i', default=9)
-parser.add_argument('--output_dim', '-o', default=6)
-args = parser.parse_args()
+config = {}
+config['model'] = root_path / 'baboon_tracking' / 'models' / 'particle_filter' / 'net.pth'
+config['input_dim'] = 9
+config['output_dim'] = 6
 
-#checks
-#check if model exists
-if not os.path.isfile(args.model):
-    assert RuntimeError(f'The given model {args.model} does not exist')
-
+########## COPY THE BELOW CODE TO THE TOP OF THE FILE WHEN MERGING PARTICLE FILTER CODE ##########
 # Setup GPU optimization if CUDA is supported
 use_cuda = torch.cuda.is_available()
 if use_cuda:
@@ -30,10 +24,20 @@ else: # Otherwise, train on the CPU
     computing_device = torch.device("cpu")
     extras = False
     print("CUDA NOT supported")
-
-
+    
 # Initialize network
-net = Nnet(args.input_dim, args.output_dim).to(computing_device)
-state_dict = torch.load(args.model, map_location=computing_device)
+net = Nnet(config['input_dim'], config['output_dim']).to(computing_device)
+state_dict = torch.load(config['model'], map_location=computing_device)
 net.load_state_dict(state_dict)
+
+########## COPY THE ABOVE CODE TO THE TOP OF THE FILE WHEN MERGING PARTICLE FILTER CODE ##########
+
+
+def predict(weights, net):
+    pass
+
+
+
+
+
 
