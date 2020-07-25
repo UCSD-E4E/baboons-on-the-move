b76f8d6c4f9b3a9631d9a37e91df34b02d5bd448
Alvin Wang <alvinwang922@gmail.com>
2020-07-24 20:21:46 -0700

    Updated pyenv management to be a bash script instead of a python script

--- scripts/pyenv-management.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.system('brew install pyenv')
-os.system('pyenv versions')
-os.system('pyenv install 3.8.3')
-os.system('pyenv global 3.8.3')
-os.system('python -v')
---
+++ scripts/pyenv-management.sh
@@ -0,0 +1,5 @@
+brew install pyenv
+pyenv versions
+pyenv install 3.8.3
+pyenv global 3.8.3
+python -v
