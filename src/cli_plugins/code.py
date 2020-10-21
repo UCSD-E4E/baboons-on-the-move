042226b314622826b597b3ee86e2cb82c8b073bf
Anh Ngo <code@anhngo.me>
2020-10-21 13:51:00 -0700

    warning instead of crashing when can't install vscode plugin

--- src/cli_plugins/code.py
+++ src/cli_plugins/code.py
@@ -3,6 +3,7 @@ Ensures that Visual Studio Code has the necessary Python extensions then launche
 """
 import os
 import subprocess
+from colorama import Fore, Style
 
 
 def _check_vscode_plugin(plugin: str):
@@ -14,7 +15,10 @@ def _check_vscode_plugin(plugin: str):
 
 def _ensure_vscode_plugin(plugin: str):
     if not _check_vscode_plugin(plugin):
-        subprocess.check_call(["code", "--install-extension", plugin])
+        try:
+            subprocess.check_call(["code", "--install-extension", plugin])
+        except Exception as e:
+            print(Fore.RED, "WARNING: PLUGIN ", plugin, " NOT INSTALLED: ", str(e), Style.RESET_ALL)
 
 
 def code():
