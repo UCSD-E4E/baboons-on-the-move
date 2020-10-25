520f39d81d9955cf705185b90795ca6d433caba6
Chris Crutchfield <ccrutchf@ucsd.edu>
2020-10-25 15:55:38 -0700

    formatted

--- src/cli_plugins/install.py
+++ src/cli_plugins/install.py
@@ -31,7 +31,9 @@ def _install_global_package(package_name: str):
     if os.getenv("VIRTUAL_ENV"):
         subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
     else:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "pipx", "--user"])
+        subprocess.check_call(
+            [sys.executable, "-m", "pip", "install", "pipx", "--user"]
+        )
         subprocess.check_call([sys.executable, "-m", "pipx", "install", package_name])
         subprocess.check_call([sys.executable, "-m", "pipx", "ensurepath"])
 
