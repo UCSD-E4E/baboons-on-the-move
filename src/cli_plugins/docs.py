c6a017f6d9363f68511d1e0fa77ec7244187fd04
Anh Ngo <code@anhngo.me>
2020-11-25 18:18:15 -0800

    pass lint checks

--- src/cli_plugins/docs.py
+++ src/cli_plugins/docs.py
@@ -11,14 +11,21 @@ def docs():
     Lints all the Python files.
     """
     if os.getenv("CLI_ACTIVE"):
-        subprocess.check_call(["make", "clean"], cwd='./docs/')
+        subprocess.check_call(["make", "clean"], cwd="./docs/")
 
         packages = ["baboon_tracking", "pipeline", "scripts"]
 
         for p in packages:
-            subprocess.check_call(["sphinx-apidoc", "-o", os.path.join('./docs/src/', p), os.path.join('./src/', p)])
+            subprocess.check_call(
+                [
+                    "sphinx-apidoc",
+                    "-o",
+                    os.path.join("./docs/src/", p),
+                    os.path.join("./src/", p),
+                ]
+            )
 
-        subprocess.check_call(["make", "html"], cwd='./docs/')
+        subprocess.check_call(["make", "html"], cwd="./docs/")
 
     else:
         os.environ["CLI_ACTIVE"] = "1"
