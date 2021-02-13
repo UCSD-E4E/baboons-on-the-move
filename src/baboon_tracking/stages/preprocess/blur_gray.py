f76779310ef489435083aa0a344c2719c433fdb2
Chris Crutchfield <ccrutchf@ucsd.edu>
2021-02-13 00:00:36 -0800

    update config

--- src/baboon_tracking/stages/preprocess/blur_gray.py
+++ src/baboon_tracking/stages/preprocess/blur_gray.py
@@ -5,21 +5,23 @@ Blurs a gray frame using a Gaussian blur.
 import cv2
 from baboon_tracking.mixins.preprocessed_frame_mixin import PreprocessedFrameMixin
 from baboon_tracking.models.frame import Frame
-from pipeline.decorators import stage
+from pipeline.decorators import stage, config
 from pipeline import Stage
 from pipeline.stage_result import StageResult
 
 
+@config(parameter_name="kernel_size", key="preprocess/kernel_size")
 @stage("preprocessed_frame")
 class BlurGray(Stage, PreprocessedFrameMixin):
     """
     Blurs a gray frame using a Gaussian blur.
     """
 
-    def __init__(self, preprocessed_frame: PreprocessedFrameMixin):
+    def __init__(self, kernel_size: int, preprocessed_frame: PreprocessedFrameMixin):
         PreprocessedFrameMixin.__init__(self)
         Stage.__init__(self)
 
+        self._kernel_size = kernel_size
         self._preprocessed_frame = preprocessed_frame
 
     def execute(self) -> StageResult:
@@ -29,7 +31,9 @@ class BlurGray(Stage, PreprocessedFrameMixin):
 
         self.processed_frame = Frame(
             cv2.GaussianBlur(
-                self._preprocessed_frame.processed_frame.get_frame(), (5, 5), 0
+                self._preprocessed_frame.processed_frame.get_frame(),
+                (self._kernel_size, self._kernel_size),
+                0,
             ),
             self._preprocessed_frame.processed_frame.get_frame_number(),
         )
