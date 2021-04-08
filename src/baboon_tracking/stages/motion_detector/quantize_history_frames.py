653722ddb3d9b24339ffe782fbad53651559c0db
Ryan Nishimoto <ryan.h.nishimoto@gmail.com>
2021-04-08 15:44:17 -0700

    quantize using numba vectorization

--- src/baboon_tracking/stages/motion_detector/quantize_history_frames.py
+++ src/baboon_tracking/stages/motion_detector/quantize_history_frames.py
@@ -1,6 +1,8 @@
 """Quantizes the shifted history frame."""
 
 import numpy as np
+import math
+from numba import vectorize
 from baboon_tracking.mixins.shifted_history_frames_mixin import (
     ShiftedHistoryFramesMixin,
 )
@@ -9,6 +11,14 @@ from baboon_tracking.mixins.quantized_frames_mixin import QuantizedFramesMixin
 from pipeline.decorators import config, stage
 from pipeline.stage import Stage
 from pipeline.stage_result import StageResult
+
+
+@vectorize("i4(u1, i8)", nopython=True, target="cpu", cache=True)
+def vector_quantize(value, scale_factor):
+    """
+    pointwise quantize operation for numba vectorization
+    """
+    return math.floor(value * scale_factor /255.0)
 
 
 @config(parameter_name="scale_factor", key="quantize_frames/scale_factor")
@@ -36,9 +46,9 @@ class QuantizeHistoryFrames(Stage, QuantizedFramesMixin):
 
     def execute(self) -> StageResult:
         """Quantizes the shifted history frame."""
+        #self._shifted_history_frames.shifted_history_frames_list = [f.get_frame() for f in self._shifted_history_frames.shifted_history_frames]
         self.quantized_frames = [
-            self._quantize_frame(f)
-            for f in self._shifted_history_frames.shifted_history_frames
+            vector_quantize(f.get_frame(), self._scale_factor) for f in self._shifted_history_frames.shifted_history_frames
         ]
 
         return StageResult(True, True)
