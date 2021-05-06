def29cc72c7cda082774eba6c91aa261dd108d1a
Chris Crutchfield <ccrutchf@ucsd.edu>
2021-05-06 02:13:49 -0700

    update baseline

--- src/baboon_tracking/stages/preprocess/preprocess_frame.py
+++ src/baboon_tracking/stages/preprocess/preprocess_frame.py
@@ -4,6 +4,8 @@ Provides a pipeline step for preprocessing a frame.
 
 from baboon_tracking.stages.preprocess.blur_gray import BlurGray
 from baboon_tracking.stages.preprocess.convert_from_bgr2gray import ConvertFromBGR2Gray
+
+from baboon_tracking.stages.preprocess.denoise import Denoise
 from baboon_tracking.stages.show_last_frame import ShowLastFrame
 from pipeline import Serial
 
@@ -15,5 +17,10 @@ class PreprocessFrame(Serial):
 
     def __init__(self):
         Serial.__init__(
-            self, "PreprocessFrame", ConvertFromBGR2Gray, BlurGray, ShowLastFrame,
+            self,
+            "PreprocessFrame",
+            ConvertFromBGR2Gray,
+            BlurGray,
+            Denoise,
+            ShowLastFrame,
         )
