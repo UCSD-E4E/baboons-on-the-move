57a038e3feb811bbd01ba45da3a4a4d994318088
Chris Crutchfield <ccrutchf@ucsd.edu>
2021-06-11 13:25:34 -0700

    jit group filter

--- src/baboon_tracking/stages/motion_detector/group_filter.py
+++ src/baboon_tracking/stages/motion_detector/group_filter.py
@@ -4,11 +4,53 @@ Implements a group filter to ensure that all pixels are in a group at least size
 
 from typing import Tuple
 import numpy as np
+from numba import jit, cuda
 from baboon_tracking.mixins.moving_foreground_mixin import MovingForegroundMixin
 from baboon_tracking.models.frame import Frame
 from pipeline import Stage
 from pipeline.decorators import config, stage
 from pipeline.stage_result import StageResult
+
+
+@jit(nopython=True)
+def _pixel_has_neighbors(moving_foreground, group_size: int, coord: Tuple[int, int]):
+    x_coord, y_coord = coord
+
+    height, width = moving_foreground.shape
+
+    if moving_foreground[y_coord, x_coord] == 0:
+        return False
+
+    min_x = max(x_coord - 1, 0)
+    max_x = min(x_coord + 1, width - 1)
+
+    min_y = max(y_coord - 1, 0)
+    max_y = min(y_coord + 1, height - 1)
+
+    count = 0
+    for y in range(min_y, max_y + 1):
+        for x in range(min_x, max_x + 1):
+            if y == y_coord and x == x_coord:
+                continue
+
+            if moving_foreground[y, x] > 0:
+                count += 1
+
+            if count >= group_size:
+                return True
+
+    return False
+
+
+@jit(nopython=True)
+# @cuda.jit()
+def _execute(
+    moving_foreground, curr_moving_foreground, group_size: int, height: int, width: int
+):
+    for y in range(height):
+        for x in range(width):
+            if _pixel_has_neighbors(curr_moving_foreground, group_size, (x, y),):
+                moving_foreground[y, x] = 255
 
 
 @config("group_size", "motion_detector/group_filter/size")
@@ -27,45 +69,27 @@ class GroupFilter(Stage, MovingForegroundMixin):
         self._group_size = group_size
         self._moving_foreground = moving_foreground
 
-    def _pixel_has_neighbors(self, coord: Tuple[int, int]):
-        x_coord, y_coord = coord
-        moving_foreground = self._moving_foreground.moving_foreground.get_frame()
-
-        height, width = moving_foreground.shape
-
-        if moving_foreground[y_coord, x_coord] == 0:
-            return False
-
-        min_x = max(x_coord - 1, 0)
-        max_x = min(x_coord + 1, width - 1)
-
-        min_y = max(y_coord - 1, 0)
-        max_y = min(y_coord + 1, height - 1)
-
-        count = 0
-        for y in range(min_y, max_y + 1):
-            for x in range(min_x, max_x + 1):
-                if y == y_coord and x == x_coord:
-                    continue
-
-                if moving_foreground[y, x] > 0:
-                    count += 1
-
-                if count >= self._group_size:
-                    return True
-
-        return False
-
     def execute(self) -> StageResult:
         moving_foreground = np.zeros_like(
             self._moving_foreground.moving_foreground.get_frame()
         )
         height, width = moving_foreground.shape
 
-        for y in range(height):
-            for x in range(width):
-                if self._pixel_has_neighbors((x, y)):
-                    moving_foreground[y, x] = 255
+        # _execute[5, 256](
+        #     moving_foreground,
+        #     self._moving_foreground.moving_foreground.get_frame(),
+        #     self._group_size,
+        #     height,
+        #     width,
+        # )
+
+        _execute(
+            moving_foreground,
+            self._moving_foreground.moving_foreground.get_frame(),
+            self._group_size,
+            height,
+            width,
+        )
 
         self.moving_foreground = Frame(
             moving_foreground,
