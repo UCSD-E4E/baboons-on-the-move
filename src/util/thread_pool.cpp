e9efc35da303269325df3ba5db2efe4733b6489d
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2021-08-11 16:43:04 -0700

    Add thread pool

---
+++ src/util/thread_pool.cpp
@@ -0,0 +1,51 @@
+#include "thread_pool.h"
+
+namespace baboon_tracking {
+thread_pool::thread_pool(std::size_t max_tasks, std::size_t thread_count)
+    : max_tasks{max_tasks} {
+  for (size_t i = 0; i < thread_count; ++i) {
+    // start waiting threads. Workers listen for changes through
+    //  the thread_pool member condition_variable
+    threads.emplace_back(std::thread([&]() {
+      std::unique_lock queue_lock(task_mutex, std::defer_lock);
+
+      while (true) {
+        queue_lock.lock();
+        task_cv.wait(queue_lock,
+                     [&]() { return !tasks.empty() || stop_threads; });
+
+        // Used by dtor to stop all threads without having to
+        // unceremoniously stop tasks. The tasks must all be
+        // finished, lest we break a promise and risk a `future`
+        // object throwing an exception.
+        if (stop_threads && tasks.empty())
+          return;
+
+        // To initialize temp_task, we must move the unique_ptr
+        // from the queue to the local stack. Since a unique_ptr
+        // cannot be copied (obviously), it must be explicitly
+        // moved. This transfers ownership of the pointed-to
+        // object to *this, as specified in 20.11.1.2.1
+        // [unique.ptr.single.ctor].
+        auto temp_task = std::move(tasks.front());
+
+        tasks.pop();
+        queue_lock.unlock();
+
+        // max_tasks_cv.notify_all();
+
+        (*temp_task)();
+      }
+    }));
+  }
+}
+
+thread_pool::~thread_pool() {
+  stop_threads = true;
+  task_cv.notify_all();
+
+  for (std::thread &thread : threads) {
+    thread.join();
+  }
+}
+} // namespace baboon_tracking
---
+++ src/util/thread_pool.h
@@ -0,0 +1,95 @@
+#pragma once
+
+#include <condition_variable>
+#include <cstdint>
+#include <functional> //bind
+#include <future>     //packaged_task
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <type_traits> //invoke_result
+#include <vector>
+
+namespace baboon_tracking {
+class thread_pool {
+public:
+  thread_pool(std::size_t max_task,
+              std::size_t thread_count = std::thread::hardware_concurrency());
+  ~thread_pool();
+
+  // F is Callable, and invoking F with ...Args should be well-formed
+  template <typename F, typename... Args>
+  auto execute(F function, Args &&...args) {
+    // std::unique_lock<std::mutex> queue_lock(task_mutex, std::defer_lock);
+    std::packaged_task<std::invoke_result_t<F, Args...>()> task_pkg(
+        [f = std::move(function),
+         fargs = std::make_tuple(std::forward<Args>(args)...)]() mutable {
+          return std::apply(std::move(f), std::move(fargs));
+        });
+    std::future<std::invoke_result_t<F, Args...>> future =
+        task_pkg.get_future();
+
+    {
+      std::unique_lock lk{task_mutex};
+      // max_tasks_cv.wait(lk, [&]() { return tasks.size() < max_tasks; });
+      // This lambda move-captures the packaged_task declared above. Since the
+      // packaged_task type is not CopyConstructible, the function is not
+      // CopyConstructible either)— hence the need for a task_container to wrap
+      // around it.
+      tasks.emplace(std::unique_ptr<task_container_base>(new task_container(
+          [task = std::move(task_pkg)]() mutable { task(); })));
+    }
+
+    task_cv.notify_one();
+
+    return std::move(future);
+  }
+
+private:
+  //  task_container_base and task_container exist simply as a wrapper around a
+  //  MoveConstructible—but not CopyConstructible—Callable object. Since an
+  //  std::function requires a given Callable to be CopyConstructible, we cannot
+  //  construct one from a lambda function that captures a non-CopyConstructible
+  //  object (such as the packaged_task declared in execute)—because a lambda
+  //  capturing a non-CopyConstructible object is not CopyConstructible.
+
+  //  task_container_base exists only to serve as an abstract base for
+  //  task_container. The dynamic dispatch is required to erase the type of the
+  //  callable.
+  class task_container_base {
+  public:
+    virtual ~task_container_base(){};
+
+    virtual void operator()() = 0;
+  };
+
+  //  task_container takes a typename F, which must be Callable and
+  //  MoveConstructible. Furthermore, F must be callable with no arguments; it
+  //  can, for example, be a bind object with no placeholders. F may or may not
+  //  be CopyConstructible.
+  template <typename F> class task_container : public task_container_base {
+  public:
+    //  Here, std::forward is needed because we need the construction of f *not*
+    //  to bind an lvalue reference—it is not a guarantee that an object of type
+    //  F is CopyConstructible, only that it is MoveConstructible.
+    task_container(F &&func) : f{std::forward<F>(func)} {}
+
+    void operator()() override { f(); }
+
+  private:
+    F f;
+  };
+
+  // Deduction guide so that we can infer task_container from some callable F
+  template <typename F> task_container(F) -> task_container<std::decay_t<F>>;
+
+  std::vector<std::thread> threads;
+  std::queue<std::unique_ptr<task_container_base>> tasks;
+  std::mutex task_mutex;
+  std::condition_variable task_cv;
+  bool stop_threads = false;
+
+  std::condition_variable max_tasks_cv;
+  std::size_t max_tasks;
+};
+} // namespace baboon_tracking
