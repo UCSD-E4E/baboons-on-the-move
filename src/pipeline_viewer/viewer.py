70ec23db8d25151980e26ab198d011da49af74d7
Christopher Crutchfield <ccrutchf@ucsd.edu>
2021-11-20 01:59:18 -0800

    improve chart viewer

--- src/pipeline_viewer/viewer.py
+++ src/pipeline_viewer/viewer.py
@@ -1,6 +1,11 @@
 import tkinter as tk
+import tkinter.ttk as ttk
+from pipeline.parent_stage import ParentStage
 
 from pipeline.pipeline import Pipeline
+from pipeline.serial import Serial
+from pipeline.parallel import Parallel
+from pipeline.stage import Stage
 
 
 class PipelineViewer:
@@ -8,7 +13,32 @@ class PipelineViewer:
         self._pipeline = pipeline
 
     def run(self):
+        title = type(self._pipeline).__name__
+
         root = tk.Tk()
-        root.title(type(self._pipeline).__name__)
+        root.title(title)
+
+        tree = ttk.Treeview(root)
+        tree.pack(fill=tk.BOTH, expand=True)
+
+        self.add_stage(tree, "", self._pipeline.stage)
 
         root.mainloop()
+
+    def add_stage(self, tree: ttk.Treeview, parent: str, stage: Stage):
+        name = type(stage).__name__
+
+        if isinstance(stage, ParentStage):
+            name = stage.name
+
+            if isinstance(stage, Serial):
+                name += " (Serial)"
+
+            if isinstance(stage, Parallel):
+                name += " (Parallel)"
+
+        parent = tree.insert(parent, index="end", iid=name, text=name)
+
+        if isinstance(stage, ParentStage):
+            for child in stage.stages:
+                self.add_stage(tree, parent, child)
