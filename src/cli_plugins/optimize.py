e2bae80ba1b9c75f74d9b1dfe5c588dc091abbeb
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2022-06-22 23:29:04 -0700

    remember to remove working idx in optimize

--- src/cli_plugins/optimize.py
+++ src/cli_plugins/optimize.py
@@ -326,6 +326,10 @@ class Optimize(CliPlugin):
             current_idx_ref.set(current_idx)
             last_update_ref.set(datetime.utcnow().isoformat())
 
+            working_idx = set(working_idx_ref.get() or [])
+            working_idx.remove(int(idx))
+            working_idx_ref.set(working_idx)
+
             if self._progress and idx in known_idx:
                 self._progressbar.n = len(current_idx)
                 self._progressbar.refresh()
