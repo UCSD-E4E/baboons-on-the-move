259a68610af8663da0096a260f561a5d3d36eca5
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2022-08-08 15:43:07 -0700

    calculate metrics will now calculate and store results.db

--- src/library/dataset.py
+++ src/library/dataset.py
@@ -3,71 +3,8 @@ Module for getting data from the NAS.
 """
 
 import pickle
-import sys
-from os import getenv, makedirs
-import getpass
-import json
 from genericpath import exists, isdir
-from synology_api import filestation
-from tqdm import tqdm
-
-
-def _download_folder(
-    synology_path: str, target_dir: str, file_station: filestation.FileStation
-):
-    makedirs(target_dir, exist_ok=True)
-
-    files_and_dirs = file_station.get_file_list(synology_path)["data"]["files"]
-    dirs = [fd for fd in files_and_dirs if fd["isdir"]]
-
-    for directory in dirs:
-        _download_folder(
-            directory["path"], f"{target_dir}/{directory['name']}", file_station
-        )
-
-    files = [fd for fd in files_and_dirs if not fd["isdir"]]
-
-    for file in tqdm(files):
-        file_station.get_file(file["path"], "download", dest_path=f"{target_dir}")
-
-
-def _get_credentials():
-    username = getenv("NAS_USER_NAME")
-    password = getenv("NAS_PASSWORD")
-
-    username_token = None
-    password_token = None
-    if exists("./nas_token.pickle"):
-        with open("./nas_token.pickle", "rb") as f:
-            username_token, password_token = pickle.load(f)
-
-    if not username:
-        if username_token:
-            username = username_token
-        else:
-            print("NAS Username: ", end="")
-            username = sys.stdin.readline().strip()
-
-    if not password:
-        if password_token:
-            password = password_token
-        else:
-            password = getpass.getpass(prompt="NAS Password: ")
-
-    with open("./nas_token.pickle", "wb") as f:
-        pickle.dump((username, password), f)
-
-    return username, password
-
-
-def _get_nas_parameters():
-    username, password = _get_credentials()
-    with open("./decrypted/nas.json", "r", encoding="utf8") as f:
-        nas_config = json.load(f)
-        hostname = nas_config["hostname"]
-        port = nas_config["port"]
-
-    return username, password, hostname, port
+from library.nas import NAS
 
 
 def get_dataset_path(name: str):
@@ -78,35 +15,10 @@ def get_dataset_path(name: str):
     dataset_root = f"./data/Datasets/{name}"
 
     if not isdir(dataset_root):
-        username, password, hostname, port = _get_nas_parameters()
-
-        file_station = filestation.FileStation(
-            hostname,
-            port,
-            username,
-            password,
-            secure=True,
-            cert_verify=False,
-            dsm_version=2,
-            debug=False,
-            otp_code=None,
-        )
-
-        _download_folder(f"/baboons/Datasets/{name}", dataset_root, file_station)
+        nas = NAS()
+        nas.download_folder(f"/baboons/Datasets/{name}", dataset_root)
 
     return dataset_root
-
-
-def _find_leaf_nodes(path: str, file_station: filestation.FileStation):
-    files_and_dirs = file_station.get_file_list(path)["data"]["files"]
-    dirs = [fd for fd in files_and_dirs if fd["isdir"]]
-    files = [fd for fd in files_and_dirs if not fd["isdir"]]
-
-    leaf_nodes = [f["path"] for f in files]
-    for directory in dirs:
-        leaf_nodes.extend(_find_leaf_nodes(directory["path"], file_station))
-
-    return leaf_nodes
 
 
 def get_dataset_list(root: str = ""):
@@ -116,21 +28,8 @@ def get_dataset_list(root: str = ""):
         with open(cache_path, "rb") as f:
             return pickle.load(f)
 
-    username, password, hostname, port = _get_nas_parameters()
-
-    file_station = filestation.FileStation(
-        hostname,
-        port,
-        username,
-        password,
-        secure=True,
-        cert_verify=False,
-        dsm_version=2,
-        debug=False,
-        otp_code=None,
-    )
-
-    leaf_nodes = _find_leaf_nodes(f"/baboons/Datasets/{root}", file_station)
+    nas = NAS()
+    leaf_nodes = nas.find_leaf_nodes(f"/baboons/Datasets/{root}")
 
     viso_datasets = set("/".join(l.split("/")[:-1]) for l in leaf_nodes)
     viso_datasets = [
---
+++ src/library/nas.py
@@ -0,0 +1,115 @@
+import json
+import pickle
+from genericpath import exists
+from os import getenv, makedirs
+import sys
+import getpass
+from synology_api import filestation
+from tqdm import tqdm
+
+
+class NAS:
+    def __init__(self):
+        username, password, hostname, port = self._get_nas_parameters()
+
+        self._file_station = filestation.FileStation(
+            hostname,
+            port,
+            username,
+            password,
+            secure=True,
+            cert_verify=False,
+            dsm_version=2,
+            debug=False,
+            otp_code=None,
+        )
+
+    def _get_credentials(self):
+        username = getenv("NAS_USER_NAME")
+        password = getenv("NAS_PASSWORD")
+
+        username_token = None
+        password_token = None
+        if exists("./nas_token.pickle"):
+            with open("./nas_token.pickle", "rb") as f:
+                username_token, password_token = pickle.load(f)
+
+        if not username:
+            if username_token:
+                username = username_token
+            else:
+                print("NAS Username: ", end="")
+                username = sys.stdin.readline().strip()
+
+        if not password:
+            if password_token:
+                password = password_token
+            else:
+                password = getpass.getpass(prompt="NAS Password: ")
+
+        with open("./nas_token.pickle", "wb") as f:
+            pickle.dump((username, password), f)
+
+        return username, password
+
+    def _get_nas_parameters(self):
+        username, password = self._get_credentials()
+        with open("./decrypted/nas.json", "r", encoding="utf8") as f:
+            nas_config = json.load(f)
+            hostname = nas_config["hostname"]
+            port = nas_config["port"]
+
+        return username, password, hostname, port
+
+    def find_leaf_nodes(self, path: str):
+        files_and_dirs = self._file_station.get_file_list(path)["data"]["files"]
+        dirs = [fd for fd in files_and_dirs if fd["isdir"]]
+        files = [fd for fd in files_and_dirs if not fd["isdir"]]
+
+        leaf_nodes = [f["path"] for f in files]
+        for directory in dirs:
+            leaf_nodes.extend(self.find_leaf_nodes(directory["path"]))
+
+        return leaf_nodes
+
+    def download_folder(self, synology_path: str, target_dir: str):
+        makedirs(target_dir, exist_ok=True)
+
+        files_and_dirs = self._file_station.get_file_list(synology_path)["data"][
+            "files"
+        ]
+        dirs = [fd for fd in files_and_dirs if fd["isdir"]]
+
+        for directory in dirs:
+            self.download_folder(directory["path"], f"{target_dir}/{directory['name']}")
+
+        files = [fd for fd in files_and_dirs if not fd["isdir"]]
+
+        for file in tqdm(files):
+            self._file_station.get_file(
+                file["path"], "download", dest_path=f"{target_dir}"
+            )
+
+    def exists(self, path: str):
+        result = self._file_station.get_file_list(path)
+
+        if "success" not in result:
+            return False
+
+        return result["success"]
+
+    def create_folder(self, path: str):
+        if self.exists(path):
+            return True
+
+        parent = "/".join(path.split("/")[:-1])
+        folder = path.split("/")[-1]
+        if not self.create_folder(parent):
+            return False
+
+        result = self._file_station.create_folder(parent, folder)
+
+        return "success" in result and result["success"]
+
+    def upload_file(self, synology_folder: str, local_file: str):
+        self._file_station.upload_file(synology_folder, local_file)
