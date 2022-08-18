fix metric calculation

src/cli_plugins/optimize.py
@@ -33,6 +33,8 @@ from library.dataset import (
 )
 from library.design_space import get_design_space
 from library.firebase import initialize_app, get_dataset_ref
+from ..library.region_file import region_factory
+from ..library.metrics import Metrics
 from library.nas import NAS
 from library.region import bb_intersection_over_union
 
@@ -103,80 +105,6 @@ class Optimize(CliPlugin):
         else:
             print(output)
 
-    def _get_metrics(self, results_db_path: str, ground_truth_path: str):
-        with connect(results_db_path) as connection:
-            cursor = connection.cursor()
-
-            ground_truth = pd.read_csv(ground_truth_path).to_numpy()
-
-            found_regions = cursor.execute(
-                "SELECT x1, y1, x2, y2, identity, frame FROM regions ORDER BY frame"
-            )
-
-            curr_frame = -1
-            true_positive = 0
-            false_negative = 0
-            false_positive = 0
-            truth = None
-            identity_map = {}
-            for x1, y1, x2, y2, identity, frame in found_regions:
-                if curr_frame != frame:
-                    if truth is not None:
-                        false_negative += truth.shape[0]
-
-                    truth = np.array(
-                        [
-                            (truth_identity, x1, y1, x1 + width, y1 + height)
-                            for truth_identity, x1, y1, width, height in ground_truth[
-                                ground_truth[:, 0] == frame, 1:6
-                            ]
-                        ]
-                    )
-                    curr_frame = frame
-
-                current = (x1, y1, x2, y2)
-                if identity not in identity_map or not np.any(
-                    truth[:, 0] == identity_map[identity]
-                ):
-                    matches = np.array(
-                        [bb_intersection_over_union(current, t[1:]) for t in truth]
-                    )
-                    if matches.size:
-                        match_idx = np.argmax(matches)
-                        score = matches[match_idx]
-                        truth_identity = truth[match_idx, 0]
-                    else:
-                        score = 0
-
-                    if score > 0 and self._tracking_enabled:
-                        identity_map[identity] = truth_identity
-                else:
-                    truth_identity = identity_map[identity]
-                    match_idx = np.argmax(truth[:, 0] == truth_identity)
-                    score = bb_intersection_over_union(current, truth[match_idx, 1:])
-
-                if score > 0:
-                    true_positive += 1
-                    truth = np.delete(truth, match_idx, 0)
-                else:
-                    false_positive += 1
-
-        if true_positive == 0:
-            false_negative = ground_truth.shape[0]
-
-        recall = true_positive / (false_negative + true_positive)
-        if false_positive == 0 and true_positive == 0:
-            precision = 0
-        else:
-            precision = true_positive / (true_positive + false_positive)
-
-        if recall == 0 or precision == 0:
-            f1 = 0
-        else:
-            f1 = (2 * recall * precision) / (recall + precision)
-
-        return recall, precision, f1
-
     def _get_score(
         self,
         X: np.ndarray,
@@ -241,9 +169,10 @@ class Optimize(CliPlugin):
                     video_file, enable_tracking, enable_persist, idx, config_hash
                 )
 
-                recall, precision, f1 = self._get_metrics(
-                    "./output/results.db", ground_truth_path
-                )
+                recall, precision, f1 = Metrics(
+                    region_factory("./output/results.db"),
+                    region_factory(ground_truth_path),
+                ).calculate_metrics()
 
                 cache_result_ref.set((recall, precision, f1))
                 cache_known_idx.append(int(idx))

src/library/metrics.py
@@ -1,97 +1,38 @@
-# """
-# Module for calculating metrics.
-# """
-# from typing import List
-# import numpy as np
-# from tqdm import tqdm
-
-# from baboon_tracking import MotionTrackerPipeline
-# from baboon_tracking.mixins.baboons_mixin import BaboonsMixin
-# from library.labeled_data import get_regions_from_xml
-# from library.region import check_if_same_region
+from typing import Dict, Iterator, List, Tuple
+from ..baboon_tracking.models.region import Region
+from library.region_file import RegionFile
+from tqdm import tqdm
 
 
-# class Metric:
-#     """Class for keeping track of a metric."""
+class Metrics:
+    def __init__(
+        self, calculated: RegionFile, ground_truth: RegionFile, threshold: float = 0
+    ):
+        self._calculated = calculated
+        self._ground_truth = ground_truth
+        self._threshold = threshold
 
-#     def __init__(self, true_positive: int, false_negative: int, false_positive: int):
-#         self.true_positive: int = true_positive
-#         self.false_negative: int = false_negative
-#         self.false_positive: int = false_positive
+    def calculate_metrics(self):
+        true_positive = 0
+        false_negative = 0
+        false_positive = 0
 
+        for calc, gd in tqdm(zip(self._calculated, self._ground_truth)):
+            for c in calc:
+                regions = [(r, c.iou(r)) for r in gd]
+                regions = [(g, i) for g, i in regions if i > 0 and i >= self._threshold]
 
-# def calculate_loss(metrics: List[Metric]):
-#     """
-#     Calculate a single number that represents the loss encurred by a set of metrics.
-#     """
+                true_positive += 1 if regions else 0
+                false_positive += 1 if not regions else 0
 
-#     false_positive = np.array([m.false_positive for m in metrics], dtype=np.float) * 1.2
-#     false_negative = np.array([m.false_negative for m in metrics], dtype=np.float) * 1.0
+            for g in gd:
+                regions = [(r, g.iou(r)) for r in calc]
+                regions = [(c, i) for c, i in regions if i > 0 and i >= self._threshold]
 
-#     return np.sum(false_positive + false_negative)
+                false_negative += 1 if not regions else 0
 
+        precision = true_positive / (true_positive + false_positive)
+        recall = true_positive / (true_positive + false_negative)
+        f1 = (2 * precision * recall) / (precision + recall)
 
-# def get_metrics() -> List[Metric]:
-#     """
-#     Gets the metrics for the specified video.
-#     """
-#     runtime_config = {"display": False, "save": False}
-
-#     tqdm.write("testing on input")
-#     baboon_labels = get_regions_from_xml("./data/input.xml")
-
-#     baboon_tracker = MotionTrackerPipeline(
-#         "./data/input.mp4", runtime_config=runtime_config
-#     )
-#     tqdm.write("tracker opened")
-#     baboons_mixin: BaboonsMixin = baboon_tracker.get(BaboonsMixin)
-
-#     should_continue = True
-#     frame_counter = 0
-#     metrics: List[Metric] = []
-#     while should_continue:
-#         true_positive = 0
-#         false_positive = 0
-
-#         should_continue = baboon_tracker.step().continue_pipeline
-#         new_found_baboons = []
-#         labeled_baboons = []
-#         if baboons_mixin.baboons is not None:
-#             new_found_baboons = [b.rectangle for b in baboons_mixin.baboons]
-#         if frame_counter in baboon_labels:
-#             labeled_baboons = baboon_labels[frame_counter]
-
-#             matched_baboons = []
-#             for new_found_baboon in new_found_baboons:
-
-#                 baboon_in_labels = [
-#                     lb
-#                     for lb in labeled_baboons
-#                     if check_if_same_region(lb, new_found_baboon)
-#                 ]
-
-#                 matched_baboons.extend(baboon_in_labels)
-#                 if baboon_in_labels:
-#                     true_positive += 1
-#                 else:
-#                     false_positive += 1
-
-#                 labeled_baboons = [
-#                     lb for lb in labeled_baboons if lb not in baboon_in_labels
-#                 ]
-
-#             false_negative = len(labeled_baboons)
-
-#             metrics.append(Metric(true_positive, false_negative, false_positive))
-
-#             # exit()
-
-#             # found_mask = create_mask(new_found_baboons)
-#             # label_mask = create_mask(labeled_baboons)
-#             # metrics.append(categorize_observations(found_mask, label_mask))
-#         else:
-#             should_continue = False
-
-#         frame_counter += 1
-
-#     return metrics
+        return recall, precision, f1